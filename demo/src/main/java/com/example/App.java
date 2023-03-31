8dac36788c0564dab7bf3caff4960da965bf6cac
Prasad Deshmukh <prasaddeshmukh.j@gmail.com>
2023-03-31 11:34:23 +0530

    initial commit

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,36 @@
+
+
+package com.example;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num=sc.nextInt();
+
+     /*   int rev=0;
+        while(num!=0)
+        {
+            rev=rev*10 + num%10;
+            num=num/10;
+            
+        } */
+
+       /*  StringBuilder sbl=new StringBuilder();
+        sbl.append(num);
+        StringBuilder rev=sbl.reverse();
+        
+        System.out.println("Reversed number :"+rev);
+     */
+
+
+
+        
+
+
+    }
+}
